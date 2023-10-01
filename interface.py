8a5876b635183972f3352b9a4fcc9d4f2afbb60f
brad-mombie <brad.momberg@gmail.com>
2023-09-30 18:38:28 -0700

    fixing bugs

--- interface.py
+++ interface.py
@@ -11,25 +11,31 @@ root.configure(bg='white')
 style = ttk.Style()
 
 # Configure the custom style for the 'Okay' button
-style.configure('TButton', font=('calibri', 10, 'bold'), borderwidth='4')
-style.map('TButton',
+style.configure('Green.TButton', 
+                font=('calibri', 10, 'bold'), 
+                borderwidth='4', 
+                background='light green')
+style.map('Green.TButton',
           foreground=[('pressed', 'black'), ('active', 'black')],
           background=[('pressed', '!disabled', 'dark green'), ('active', 'light green')]
          )
 
 # Create the 'Okay' button using the custom style and place it at the bottom left
-okay_button = ttk.Button(root, text="Okay", command=lambda: print("Okay pressed"), style='TButton')
+okay_button = ttk.Button(root, text="Okay", command=lambda: print("Okay pressed"), style='Green.TButton')
 okay_button.place(x=10, y=430, width=100, height=40)
 
 # Configure the custom style for the 'Why' button
-style.configure('TButtonYellow', font=('calibri', 10, 'bold'), borderwidth='4')
-style.map('TButtonYellow',
+style.configure('Yellow.TButton', 
+                font=('calibri', 10, 'bold'), 
+                borderwidth='4', 
+                background='light yellow')
+style.map('Yellow.TButton',
           foreground=[('pressed', 'black'), ('active', 'black')],
           background=[('pressed', '!disabled', 'dark orange'), ('active', 'light yellow')]
          )
 
 # Create the 'Why' button using the custom style and place it at the bottom right
-why_button = ttk.Button(root, text="Why", command=lambda: print("Why pressed"), style='TButtonYellow')
+why_button = ttk.Button(root, text="Why", command=lambda: print("Why pressed"), style='Yellow.TButton')
 why_button.place(x=690, y=430, width=100, height=40)
 
 root.mainloop()

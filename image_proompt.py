bea822f17ed3493f82981521f46fd37f425ce85d
brad-mombie <brad.momberg@gmail.com>
2023-09-30 20:41:40 -0700

    testing

--- image_proompt.py
+++ image_proompt.py
@@ -1,32 +1,18 @@
-import requests
+import openai
 import os
 
-# Your existing code
-url = 'https://your-api-url.com/endpoint'
-headers = {
-    'Authorization': 'Bearer your_api_key_here'
-}
+# Load your API key from an environment variable or secret management service
+openai.api_key = 'your_api_key_here'
 
-# Getting the path of the image
+# Get the path of the image
 current_dir = os.path.dirname(os.path.abspath(__file__))
 image_path = os.path.join(current_dir, 'test.jpg')
 
-# Preparing the files and data for the request
-files = {'file': open(image_path, 'rb')}
-data = {'prompt': 'Please tell me about this medication.'}
+# Make the request
+response = openai.Image.create(
+    image=open(image_path, 'rb'),
+    prompt='Please tell me about this medication.'
+)
 
-# Making the request
-response = requests.post(url, headers=headers, files=files, data=data)
-
-# New troubleshooting code
-print(f'Status Code: {response.status_code}')
-print(f'Response Text: {response.text}')
-
-# Outputting the response (if it's JSON)
-if response.status_code == 200:
-    try:
-        print(response.json())
-    except Exception as e:
-        print(f'Error parsing JSON: {e}')
-else:
-    print('Request failed.')
+# Output the response
+print(response)

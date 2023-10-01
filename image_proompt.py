04910ad739f0849b18029a39a5735d23b691c03b
brad-mombie <brad.momberg@gmail.com>
2023-09-30 20:36:31 -0700

    initial image proompt

---
+++ image_proompt.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+# Your existing code
+url = 'https://your-api-url.com/endpoint'
+headers = {
+    'Authorization': 'Bearer your_api_key_here'
+}
+
+# Getting the path of the image
+current_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(current_dir, 'test.jpg')
+
+# Preparing the files and data for the request
+files = {'file': open(image_path, 'rb')}
+data = {'prompt': 'Please tell me about this medication.'}
+
+# Making the request
+response = requests.post(url, headers=headers, files=files, data=data)
+
+# Outputting the response
+print(response.json())

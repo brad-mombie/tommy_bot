81f9ae9a309c2663af731bc5833f0f4e3c0d74c0
Mohammad Nafisi <nafisidev@gmail.com>
2023-09-30 21:11:06 -0700

    new pages

--- page4.py
+++ page4.py
@@ -1,4 +1,3 @@
-# root.mainloop()
 import tkinter as tk
 from tkinter import ttk
 import signal
@@ -13,7 +12,7 @@ signal.signal(signal.SIGTERM, signal_handler)
 
 # Create the main window
 root = tk.Tk()
-root.title("User Interface")
+root.title("Michael, time for your medication")  # Updated title
 root.geometry("800x480")
 root.configure(bg='white')
 
@@ -24,32 +23,32 @@ root.attributes("-fullscreen", True)
 style = ttk.Style()
 
 # Configure the custom style for the 'Okay' button
-style.configure('Green.TButton', 
-                font=('calibri', 80, 'bold'), 
-                borderwidth='4', 
-                background='light green')
+style.configure('Green.TButton',
+font=('calibri', 80, 'bold'),
+borderwidth='4',
+background='light green')
 style.map('Green.TButton',
-          foreground=[('pressed', 'black'), ('active', 'black')],
-          background=[('pressed', '!disabled', 'dark green'), ('active', 'light green')]
-         )
+foreground=[('pressed', 'black'), ('active', 'black')],
+background=[('pressed', '!disabled', 'dark green'), ('active', 'light green')]
+)
 
 # Create the 'Okay' button using the custom style and place it at the bottom left
 okay_button = ttk.Button(root, text="Okay", command=lambda: print("Okay pressed"), style='Green.TButton')
 okay_button.place(x=20, y=660, width=800, height=320)
 
-# Configure the custom style for the 'Why' button
-style.configure('Yellow.TButton', 
-                font=('calibri', 80, 'bold'), 
-                borderwidth='4', 
-                background='light yellow')
+# Configure the custom style for the 'No' button
+style.configure('Yellow.TButton',
+font=('calibri', 80, 'bold'),
+borderwidth='4',
+background='light yellow')
 style.map('Yellow.TButton',
-          foreground=[('pressed', 'black'), ('active', 'black')],
-          background=[('pressed', '!disabled', 'dark orange'), ('active', 'light yellow')]
-         )
+foreground=[('pressed', 'black'), ('active', 'black')],
+background=[('pressed', '!disabled', 'dark orange'), ('active', 'light yellow')]
+)
 
-# Create the 'Why' button using the custom style and place it at the bottom right
-why_button = ttk.Button(root, text="Why", command=lambda: print("Why pressed"), style='Yellow.TButton')
-why_button.place(x=800, y=660, width=800, height=320)
+# Create the 'No' button using the custom style and place it at the bottom right
+no_button = ttk.Button(root, text="No", command=lambda: print("No pressed"), style='Yellow.TButton')
+no_button.place(x=800, y=660, width=800, height=320)
 
 # Add the 'sub_heading' label at the top left
 sub_heading = tk.Label(root, text="Time for your medication: ", font=('calibri', 80, 'bold'), bg='white')

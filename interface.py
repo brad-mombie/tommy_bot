c5a61fd02bd321c32c8690b483186e2ca26cb9c8
brad-mombie <brad.momberg@gmail.com>
2023-09-30 18:59:16 -0700

    Fixed button positions

--- interface.py
+++ interface.py
@@ -22,9 +22,9 @@ root.attributes("-fullscreen", True)
 # Create a style object
 style = ttk.Style()
 
-# Configure the custom style for the 'Okay' button with the previous increased font size
+# Configure the custom style for the 'Okay' button with further increased font size
 style.configure('Green.TButton', 
-                font=('calibri', 40, 'bold'), 
+                font=('calibri', 80, 'bold'), 
                 borderwidth='4', 
                 background='light green')
 style.map('Green.TButton',
@@ -34,11 +34,11 @@ style.map('Green.TButton',
 
 # Create the 'Okay' button using the custom style and place it at the bottom left with the specified size
 okay_button = ttk.Button(root, text="Okay", command=lambda: print("Okay pressed"), style='Green.TButton')
-okay_button.place(x=800, y=420, width=400, height=160)
+okay_button.place(x=10, y=160, width=800, height=320)
 
-# Configure the custom style for the 'Why' button with the previous increased font size
+# Configure the custom style for the 'Why' button with further increased font size
 style.configure('Yellow.TButton', 
-                font=('calibri', 40, 'bold'), 
+                font=('calibri', 80, 'bold'), 
                 borderwidth='4', 
                 background='light yellow')
 style.map('Yellow.TButton',
@@ -48,7 +48,7 @@ style.map('Yellow.TButton',
 
 # Create the 'Why' button using the custom style and place it at the bottom right with the specified size
 why_button = ttk.Button(root, text="Why", command=lambda: print("Why pressed"), style='Yellow.TButton')
-why_button.place(x=390, y=320, width=400, height=160)
+why_button.place(x=410, y=160, width=800, height=320)
 
 # Add the 'sub_heading' label at the top left
 sub_heading = tk.Label(root, text="Time for your medication: ", font=('calibri', 20, 'bold'), bg='white')
